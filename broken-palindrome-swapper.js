598b7c0eb09d9fb9026ca9ce403f75d6a1eaadfa
Hilarion18 <hilarion_cross@hotmail.com>
2018-09-04 12:59:52 +0700

    palin 1

---
+++ broken-palindrome-swapper.js
@@ -0,0 +1,47 @@
+/**
+ * @function palindromeSwapper
+ * menukar 2 huruf berurutan dari n = 0 sampai n-1
+ * contoh: makan => amkan, mkaan, maakn, makna
+ * @param {string} str - input kata yang ingin di-swap dan dicek palindrom
+ * @returns {boolean} true bila kata yang di-swap adalah palindrom
+ */
+function palindromeSwapper(str) {
+  if (isPalindrome(str)) return true;
+  for (var i = 0; i < str.length; i++) {
+  let newStr = '';
+    for (var j = 0; newStr.length < str.length; j++) {
+        if (i === j) {
+            newStr += str[j+1];
+            debugger
+            newStr += str[j];
+            debugger
+            j++;
+            debugger
+        } 
+        else {
+            newStr += str[j];
+            debugger
+        }
+    }
+    debugger
+    if (isPalindrome(newStr)) return true;
+}
+return false;
+}
+
+/**
+ * @function isPalindrome
+ * Cek apabila kata merupakan palindromeSwapper
+ * @param {string} str - input kata yang dicek bila palindrom
+ * @returns {boolean} true bila kata adalah palindrom
+ */
+function isPalindrome(str) {
+  if (str.split('').reverse().join('') === str) return true;
+  return false;
+}
+
+console.log(palindromeSwapper('arcecar')); // TRUE
+console.log(palindromeSwapper('racecar')); // TRUE
+console.log(palindromeSwapper('recacar')); // FALSE
+console.log(palindromeSwapper('eraccar')); // FALSE
+console.log(palindromeSwapper('raceacr')); // TRUE
